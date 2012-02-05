47737f2c995d3a1489cbf1d8c2dd33f6450af670
ruph <davor.strehar@gmail.com>
2012-02-05 18:51:50 +0100

    swank clojure instructions

--- lang-clojure.el
+++ lang-clojure.el
@@ -2,8 +2,9 @@
 (add-to-list 'load-path "~/.emacs.d/elpa/clojure-mode-1.11.5/")
 (require 'clojure-mode)
 
-(add-hook 'clojure-mode-hook 'flymake-find-file-hook)
-
-
+;; swank-clojure
+;; https://github.com/technomancy/swank-clojure
+;; lein plugin install swank-clojure 1.4.0
+;; M-x clojure-jack-in
 
 (provide 'lang-clojure)
