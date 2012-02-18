4343fb352aaba1dfb98f6720e8093aa9fdd30334
ruph <davor.strehar@gmail.com>
2012-02-18 23:27:01 +0100

    kill 2 start of the line, +new ido settings

--- init.el
+++ init.el
@@ -99,6 +99,13 @@
 (global-set-key (kbd "<f7>") 'find-name-dired)
 (global-set-key (kbd "S-<f7>") 'rgrep)
 
+;; Delete to start of the line
+(defun kill-start-of-line ()
+  "kill from point to start of line"
+  (interactive)
+  (kill-line 0))
+(global-set-key (kbd "C-;") 'kill-start-of-line)
+
 ;; Recent files
 (require 'recentf)
 (recentf-mode 1)
@@ -126,27 +133,22 @@
 
 ;; IDO mode
 ;; http://www.emacswiki.org/cgi-bin/wiki/InteractivelyDoThings
-; - old
-;(setq ido-enable-flex-matching t)
-;(setq ido-everywhere t)
-;(ido-mode 1)
-; new - testing
 (require 'ido)
 (ido-mode 'both) ;; for buffers and files
 (setq
   ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
-  ido-ignore-buffers ;; ignore these guys
-  '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
-     "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
-  ido-work-directory-list '("~/" "~/Desktop" "~/Documents" "~src")
+  ;; ido-ignore-buffers ;; ignore these guys
+  ;; '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
+  ;;    "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
+  ;; ido-work-directory-list '("~/" "~/Desktop" "~/Documents" "~src")
   ido-case-fold  t                 ; be case-insensitive
   ido-enable-last-directory-history t ; remember last used dirs
   ido-max-work-directory-list 30   ; should be enough
   ido-max-work-file-list      50   ; remember many
   ido-use-filename-at-point nil    ; don't use filename at point (annoying)
   ido-use-url-at-point nil         ; don't use url at point (annoying)
-  ido-enable-flex-matching nil     ; don't try to be too smart
-  ido-max-prospects 8              ; don't spam my minibuffer
+  ;; ido-enable-flex-matching nil     ; don't try to be too smart
+  ido-max-prospects 16             ; don't spam my minibuffer
   ido-confirm-unique-completion t) ; wait for RET, even with unique completion
 
 ;; when using ido, the confirmation is rather annoying...
