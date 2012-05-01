3bba16a5a1ee14169219a768f6dd1985a87ebfbf
ruph <davor.strehar@gmail.com>
2012-05-01 12:33:02 +0200

    org mode for *scratch*

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
   read-file-name-completion-ignore-case t) ;; ...filenames too
 
 (setq initial-scratch-message ";; scratch pad\n\n")
+(setq initial-major-mode 'org-mode)      ;; org. scratch pad
 
 (setq-default
  frame-title-format
