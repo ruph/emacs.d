e99ca00143d04f347d129ff8e9f1e170d74b8b3f
ruph <davor.strehar@gmail.com>
2012-09-29 14:23:47 +0200

    cleanup

--- lang-clojure.el
+++ lang-clojure.el
@@ -4,29 +4,23 @@
 (setq clojure-enable-paredit t)
 
 
-;; clojurescript
+;; Clojurescript
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
 
-;; swank-clojure
+;; Swank-clojure
 ;; https://github.com/technomancy/swank-clojure
 ;; lein plugin install swank-clojure 1.4.0
 ;; M-x clojure-jack-in
 
 
-;; slime autocompletion module
+;; Slime autocompletion module
 (add-to-list 'load-path "~/.emacs.d/el-get/ac-slime")
 (require 'ac-slime)
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 
 
-;; yasnippet
-;; https://github.com/swannodette/clojure-snippets
-;; git clone https://github.com/swannodette/clojure-snippets.git clojure-mode
-;; ..in snippets dir preferably
-
-
-;; curly & square brackets in paredit
+;; Curly & square brackets in paredit
 (defun setup-mode-paredit (mode-map)
   (define-key mode-map
     (kbd "DEL") 'paredit-backward-delete)
@@ -66,6 +60,6 @@
   (interactive "Directory: ")
   (shell-command
    (format "find %s -name \'*.clj*\' | xargs etags --regex=\'/[ \\t\\(]*def[a-z]* \\([a-z-!]+\\)/\\1/\' --regex=\'/[ \\t\\(]*ns \\([a-z.]+\\)/\\1/\' -o %s/TAGS" dir-name dir-name)
-))
+   ))
 
 (provide 'lang-clojure)
