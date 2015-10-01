ecc112f51970b019c0329931860679f436e2a37a
ruph <davor.strehar@gmail.com>
2015-10-01 20:42:56 +0200

    better js mode

--- lisp/lang-javascript.el
+++ lisp/lang-javascript.el
@@ -2,40 +2,27 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
 ;; tern
 (require 'tern)
 
-;; Js2-mode
+;; mode settings
+(defun js-mode-common ()
+  (setq tab-width 4)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
+  (tern-mode t)
+  (set (make-local-variable 'company-backends)
+       '((company-dabbrev-code company-tern company-keywords company-yasnippet))))
+
+(add-hook 'js-mode-hook 'js-mode-common)
+
 (add-hook 'js2-mode-hook
           (lambda ()
-            ;; (setq indent-tabs-mode t) ;; .editorconfig
-            (setq tab-width 4)
+            (js-mode-common)
             (setq js2-indent-level 4)
             (setq js2-missing-semi-one-line-override t)
             (local-unset-key (kbd "M-j")) ;; jump to window below conflict
             (yas-activate-extra-mode 'js-mode)
-            (tern-mode t)
-            (set (make-local-variable 'company-backends)
-                 '((company-dabbrev-code company-tern company-keywords company-yasnippet)))
-            ))
-
-;; some jsx support -- causes problems with `<` in `if` 
-;(defun modify-syntax-table-for-jsx ()
-;  (modify-syntax-entry ?< "(>")
-;  (modify-syntax-entry ?> ")<"))
-;(add-hook 'js2-mode-hook 'modify-syntax-table-for-jsx)
-
-;; default js-mode
-(add-hook 'js-mode-hook
-          (lambda ()
-            (setq tab-width 4)
-            (add-hook 'before-save-hook 'delete-trailing-whitespace)
-            (yas-activate-extra-mode 'js2-mode)
-            (tern-mode t)
-            (set (make-local-variable 'company-backends)
-                 '((company-dabbrev-code company-tern company-keywords company-yasnippet)))
             ))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
