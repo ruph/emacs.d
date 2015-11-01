d6c3b84aa65dd7555283bd4a8b290c42f841a3bd
ruph <davor.strehar@gmail.com>
2015-11-01 22:41:11 +0100

    fluid fill works best

--- lisp/init-irc.el
+++ lisp/init-irc.el
@@ -8,17 +8,12 @@
  '(erc-my-nick-face ((t (:foreground "indian red" :weight bold))))
  '(erc-nick-default-face ((t (:foreground "dark gray" :weight bold)))))
 
-;; http://www.emacswiki.org/emacs/ErcFilling
-(make-variable-buffer-local 'erc-fill-column)
-(add-hook 'window-configuration-change-hook
-		  '(lambda ()
-			 (save-excursion
-			   (walk-windows
-				(lambda (w)
-				  (let ((buffer (window-buffer w)))
-					(set-buffer buffer)
-					(when (eq major-mode 'erc-mode)
-					  (setq erc-fill-column (- (window-width w) 3)))))))))
+;; fluid filling works best
+(add-hook 'erc-mode-hook
+          (lambda ()
+			(erc-fill-mode nil)
+			(erc-fill-disable)
+			(visual-line-mode t)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
