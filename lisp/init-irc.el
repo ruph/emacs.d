d0c530d81e888142de688f0943820e026158aae4
ruph <davor.strehar@gmail.com>
2015-11-02 19:05:17 +0100

    moving irc stamp setting to init-irc

--- lisp/init-irc.el
+++ lisp/init-irc.el
@@ -1,12 +1,19 @@
 ;; ERC
 (setq erc-track-exclude-types '("JOIN" "KICK" "NICK" "PART" "QUIT" "333" "353"))
+(setq erc-insert-away-timestamp-function (quote erc-insert-timestamp-left))
+(setq erc-insert-timestamp-function (quote erc-insert-timestamp-left))
+(setq erc-stamp-mode t)
+(setq erc-timestamp-only-if-changed-flag nil)
+(setq erc-timestamp-use-align-to t)
 
 ;; colors
 (custom-set-faces
  '(erc-button ((t (:underline "dark gray" :weight bold))))
  '(erc-input-face ((t (:foreground "indian red"))))
  '(erc-my-nick-face ((t (:foreground "indian red" :weight bold))))
- '(erc-nick-default-face ((t (:foreground "dark gray" :weight bold)))))
+ '(erc-nick-default-face ((t (:foreground "dark gray" :weight bold))))
+ '(erc-nick-default-face ((t (:foreground "dark gray" :weight bold))))
+ '(erc-timestamp-face ((t (:foreground "dim gray" :weight normal)))))
 
 ;; fluid filling works best
 (add-hook 'erc-mode-hook
