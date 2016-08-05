15c886d3e872c978a88216b631d10b6f515fe008
ruph <davor.strehar@gmail.com>
2016-08-05 21:43:01 +0200

    run defadvice for paren-mode only on paren-mode

--- lisp/packages.el
+++ lisp/packages.el
@@ -362,16 +362,17 @@
 (set-face-attribute 'show-paren-match nil :weight 'extra-bold)
 
 (defadvice show-paren-function
-    (after show-matching-paren-offscreen activate)
+	(after show-matching-paren-offscreen activate)
   "If the matching paren is offscreen, show the matching line in the
         echo area. Has no effect if the character before point is not of
         the syntax class ')'"
   (interactive)
-  (let* ((cb (char-before (point)))
-         (matching-text (and cb
-                             (char-equal (char-syntax cb) ?\) )
-                             (blink-matching-open))))
-    (when matching-text (message matching-text))))
+  (if show-paren-mode
+	  (let* ((cb (char-before (point)))
+			 (matching-text (and cb
+								 (char-equal (char-syntax cb) ?\) )
+								 (blink-matching-open))))
+		(when matching-text (message matching-text)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
