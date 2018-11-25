2fa2ede2630e4698ce4b06e51140b7bddf818329
ruph <davor.strehar@gmail.com>
2018-11-25 11:49:43 +0100

    ggtags for go

--- lisp/lang-go.el
+++ lisp/lang-go.el
@@ -1,6 +1,8 @@
 ;; GO
 (require 'go-mode)
-(add-hook 'go-mode-hook (lambda ()
-                          (flycheck-select-checker 'go-gofmt)))
+(add-hook 'go-mode-hook
+		  (lambda ()
+            (flycheck-select-checker 'go-gofmt)
+			(ggtags-mode 1)))
 
 (provide 'lang-go)
